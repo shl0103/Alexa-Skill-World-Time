373f24c007fec6628806e71c571cd425a37cb3a6
Aiswarya Bhavani Shankar <aish2102@gmail.com>
2021-12-05 00:12:02 -0800

    Update alexa_skill_lambda.py

--- alexa_skill_lambda.py
+++ alexa_skill_lambda.py
@@ -30,7 +30,6 @@ def build_speechlet_response(title, output, should_end_session):
         'shouldEndSession': should_end_session
     }
 
-
 def build_response(session_attributes, speechlet_response):
     return {
         'version': '1.0',
@@ -56,7 +55,6 @@ def get_welcome_response():
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, should_end_session))
 
-
 def handle_session_end_request():
     card_title = "Session Ended"
     speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
@@ -66,12 +64,10 @@ def handle_session_end_request():
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
-
 def create_favorite_color_attributes(favorite_color):
     return {"favoriteColor": favorite_color}
 
-def GetMeTime(intent, session):
-    
+def GetMeTime(intent, session):   
     card_title = intent['name']
     session_attributes = {}
     should_end_session = True
@@ -155,7 +151,6 @@ def get_color_from_session(intent, session):
     return build_response(session_attributes, build_speechlet_response(
         intent['name'], speech_output, reprompt_text, should_end_session))
 
-
 # --------------- Events ------------------
 
 def on_session_started(session_started_request, session):
@@ -203,7 +198,6 @@ def on_session_ended(session_ended_request, session):
     """
     print("on_session_ended requestId=" + session_ended_request['requestId'] +
           ", sessionId=" + session['sessionId'])
-    # add cleanup logic here
 
 # --------------- Main handler ------------------
 
@@ -213,23 +207,8 @@ def lambda_handler(event, context):
     """
     print("event.session.application.applicationId=" +
           event['session']['application']['applicationId'])
-
-    """
-    Uncomment this if statement and populate with your skill's application ID to
-    prevent someone else from configuring a skill that sends requests to this
-    function.
-    """
-    # if (event['session']['application']['applicationId'] !=
-    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
-    #     raise ValueError("Invalid Application ID")
-
     if event['session']['new']:
         on_session_started({'requestId': event['request']['requestId']},
                            event['session'])
-
-    """if event['request']['type'] == "LaunchRequest":
-        return on_launch(event['request'], event['session'])"""
     if event['request']['type'] == "IntentRequest":
         return on_intent(event['request'], event['session'])
-    """elif event['request']['type'] == "SessionEndedRequest":
-        return on_session_ended(event['request'], event['session'])"""
